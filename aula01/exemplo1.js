7aa4f52bb61acfcd5708004e6e314cd9065a93c7
liviaaestp <liviaamandaestevaodepaula@gmail.com>
2026-02-24 11:38:46 -0300

    24/2

---
+++ aula01/exemplo1.js
@@ -0,0 +1,13 @@
+document.write ("<h2>Entrada de Dados </h2>");
+
+var numero1 = prompt ( "Digite o numero 1");
+var numero2 = prompt ("Digite o numero 2" );
+
+document.write ("numero 1 = " + numero1 + "<br>");
+document.write ("numero 2 = " + numero2 + "<br>");
+
+document.write ("<h2> operações aritméticas </h2>");
+var resultado;
+
+resultado = number (numero1) + number( numero2);
+document.write ("A soma dos números é " + resultado + "<br/>")
---
+++ aula01/script.js
@@ -0,0 +1,16 @@
+var nome = "Guilherme";
+var idade = 46;
+var altura = 1.82;
+
+console.log ( nome );
+console.log ( idade );
+console.log ( altura );
+
+document.write ("<h1> Olá, Seja bem vindo ao javascript </h1>")
+
+document.write ( nome + "<br>");
+document.write ( idade + "<br>");
+document.write ( altura + "<br>");
+
+var dias = idade * 365;
+document.write ("<p style='color ; red'> Você ja viveu" + dias + "dias</p>");
