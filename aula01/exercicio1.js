38ba35011f4a8d3741191653c3aa6bc3d097dd75
liviaaestp <liviaamandaestevaodepaula@gmail.com>
2026-03-03 11:35:15 -0300

    03/03

---
+++ aula01/exercicio1.js
@@ -0,0 +1,12 @@
+document.write ("<h1>Aula 01 - Exercicio 1</h1>");
+
+var numero = prompt ("Digite um número qualquer: ");
+
+var quqdrado = Number (numero) * Number (numero);
+var cubo = Number (numero) * Number (numero) * Number (numero);
+
+document.write ("<p> O numero digitado foi " + numero + "<p>");
+
+document.write ("<p style = 'color : red '> O quadrado do numero é " + quadrado + "</p>");
+document.write ("<p style = 'color: blue' > O cubo do numero é " + cubo + "</p");
+
---
+++ aula01/exercicio2.js
@@ -0,0 +1,10 @@
+document.write ("<h1> Exercicio 2 </h1>");
+
+var fahrenheit = prompt ("Digite a temperatura em fahrenheit: ");
+var celsius = Number (fahrenheit - 32)  * 5/9;
+
+document.write ("<p> A temperatura digitada foi " + fahrenheit + "F </p>");
+
+document.write ("<p style = 'color: red'> A tempertura em celsius é " + celsius +  "C</p>");
+
+
