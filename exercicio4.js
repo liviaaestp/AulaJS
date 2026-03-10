2c017c228db9f5c0a525fd9fafaf7040b64d901c
liviaaestp <liviaamandaestevaodepaula@gmail.com>
2026-03-10 11:37:12 -0300

    10/03

---
+++ exercicio4.js
@@ -0,0 +1,9 @@
+document.write("<h1>Exercicio 4 </h1>");
+
+var base= Number (prompt ("Digite o valor da base do um triangulo "));
+
+var altura = Number (prompt ("Digite o valor da altura de um triangulo "));
+
+var area = (base * altura) / 2;
+
+document.write ("<p> A area do triangulo é: " + area + "</p>");
